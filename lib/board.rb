006a0f5b9f8450b7b14947c419b375fe1c8172bc
Steven Inouye <steveninouye@msn.com>
2018-09-09 18:39:51 -1000

    completed board and human player

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,33 @@
 class Board
+    attr_reader :grid
+
+    def initialize(grid=[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]])
+        @grid = grid
+    end
+
+    def place_mark(coord, symb)
+        x, y = coord
+        @grid[x][y]= symb
+    end
+
+    def empty?(coord)
+        x, y = coord
+        @grid[x][y] == nil
+    end
+
+    def winner
+        p grid
+        # check rows and columns
+        (0..2).each do |el|
+            return grid[0][el] if grid[0][el] && grid[0][el] == grid[1][el] && grid[0][el] == grid[2][el]
+            return grid[el][0] if grid[el][0] && grid[el][0] == grid[el][1] && grid[el][0] == grid[el][2]
+        end
+        return grid[0][0] if grid[0][0] && grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2]
+        return grid[1][1] if grid[1][1] && grid[1][1] == grid[0][2] && grid[1][1] == grid[2][0]
+    end
+
+    def over?
+        return true if winner || grid.all?{|arr| arr.compact.length == 3}
+        false
+    end
 end
--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,2 +1,24 @@
 class HumanPlayer
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+    end
+
+    def get_move
+        puts "where"
+        input = gets.chomp
+        coord = input.split(", ")
+        filtered_coord = coord.select{|el| "012".include?(el)}.map{|str| str.to_i}
+        if filtered_coord.length != 2
+            puts "Not a valid coordinate"
+            get_move!
+        else
+            filtered_coord
+        end
+    end
+
+    def display(board)
+        p board
+    end
 end
